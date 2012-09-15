fd11313a792211e1974264c326f281e62cf3ebe0
Lixus3d <developpement@adreamaline.com>
2012-09-15 20:58:24 +0200

    New branch and new repository template

--- template/Gitophp/newRepository.php
+++ template/Gitophp/newRepository.php
@@ -1,5 +1,5 @@
 	<div class="titlehead container12">
-		<h1 class="container8">Create new repository</h1>
+		<h1 class="container8">Create a new repository</h1>
 		<div class="actions grid4">
 			<!--<ul>
 				<li class="button"><a href="<?=$this->getBaseUrl('Gitophp/new-repository')?>">Create new repository</a></li>
@@ -8,4 +8,6 @@
 	</div>
 	<div class="clear"></div>
 
-	TODO
+	<div class="form git-form">
+		<? echo $this->repositoryForm->render() ;?>
+	</div>
