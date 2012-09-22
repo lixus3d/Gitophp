e0010be798f2baa585106c84a9f8b0f1e5b3fb17
Lixus3d <developpement@adreamaline.com>
2012-09-22 10:29:57 +0200

    Fix smally url rewriting init

--- model/UrlRewriting.php
+++ model/UrlRewriting.php
@@ -2,7 +2,7 @@
 
 class UrlRewriting extends \Smally\AbstractUrlRewriting {
 
-	public function __construct(){
+	public function init(){
 
 		// For the repo home
 		$this->addRule('#(.*\.git)$#', array('path'=>'Gitophp/repo','matches'=>array('match','repo')));
